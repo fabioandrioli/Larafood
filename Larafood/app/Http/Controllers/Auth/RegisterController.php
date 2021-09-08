68939c1ccfec1ef747adff710c244091237a6a39
Fabio <fabio.drioli@gmail.com>
2021-09-08 13:40:42 -0300

    Criação das validações para os campos

--- Larafood/app/Http/Controllers/Auth/RegisterController.php
+++ Larafood/app/Http/Controllers/Auth/RegisterController.php
@@ -52,11 +52,11 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            // 'name' => ['required', 'string', 'max:255'],
-            // 'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            // 'password' => ['required', 'string', 'min:8', 'confirmed'],
-            // 'cnpj'=> ['required','unique:tenants'],
-            // 'empresa'=> ['required','unique:empresa,name'],
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:6', 'confirmed'],
+            'cnpj'=> ['required','unique:tenants'],
+            'empresa'=> ['required','unique:tenants,name'],
         ]);
     }
 
